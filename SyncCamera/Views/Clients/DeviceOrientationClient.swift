4776e9950f1232151815640296b21c098c9862c8
Kazuya Ueoka <info@fromkk.info>
2025-06-25 19:50:01 +0900

    add DeviceOrientationClient

---
+++ SyncCamera/Views/Clients/DeviceOrientationClient.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+struct DeviceOrientationClient: Sendable {
+  var subscribe: @Sendable () -> AsyncStream<UIDeviceOrientation>
+}
+
+extension DeviceOrientationClient {
+  static let liveValue: DeviceOrientationClient = Self(
+    subscribe: {
+      return AsyncStream { continuation in
+        let task = Task { @MainActor in
+          let device = UIDevice.current
+          device.beginGeneratingDeviceOrientationNotifications()
+          for await _ in NotificationCenter.default.notifications(named: UIDevice.orientationDidChangeNotification).map({ _ in () }) {
+            continuation.yield(UIDevice.current.orientation)
+          }
+        }
+        continuation.onTermination = { _ in
+          Task { @MainActor in
+            UIDevice.current.endGeneratingDeviceOrientationNotifications()
+          }
+          task.cancel()
+        }
+      }
+    }
+  )
+}
