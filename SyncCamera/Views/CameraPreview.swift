eeed1a863d94d65c8ff32d03bf39a3857b531330
Kazuya Ueoka <info@fromkk.info>
2025-06-25 20:05:47 +0900

    move CameraPreview

---
+++ SyncCamera/Views/CameraPreview.swift
@@ -0,0 +1,58 @@
+import AVFoundation
+import SwiftUI
+import UIKit
+
+/// AVCaptureVideoPreviewLayerをSwiftUIで表示するためのビュー
+struct CameraPreview: UIViewControllerRepresentable {
+  /// プレビュー表示用のレイヤー
+  let previewLayer: AVCaptureVideoPreviewLayer
+
+  typealias UIViewControllerType = UIViewController
+
+  class Coordinator: NSObject {
+    let previewLayer: AVCaptureVideoPreviewLayer
+    let device: AVCaptureDevice?
+
+    init(previewLayer: AVCaptureVideoPreviewLayer) {
+      self.previewLayer = previewLayer
+      self.device =
+        previewLayer.session?.inputs.compactMap { $0 as? AVCaptureDeviceInput }.first?.device
+    }
+
+    @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+      let point = gesture.location(in: gesture.view)
+      let focusPoint = previewLayer.captureDevicePointConverted(fromLayerPoint: point)
+      guard let device else { return }
+      do {
+        try device.lockForConfiguration()
+        if device.isFocusPointOfInterestSupported {
+          device.focusPointOfInterest = focusPoint
+          device.focusMode = .autoFocus
+        }
+        device.unlockForConfiguration()
+      } catch {}
+    }
+  }
+
+  func makeCoordinator() -> Coordinator {
+    Coordinator(previewLayer: previewLayer)
+  }
+
+  /// UIViewControllerの生成とプレビューレイヤー追加
+  func makeUIViewController(context: Context) -> UIViewController {
+    let vc = UIViewController()
+    vc.view.layer.addSublayer(previewLayer)
+    vc.view.addGestureRecognizer(
+      UITapGestureRecognizer(
+        target: context.coordinator, action: #selector(context.coordinator.handleTap(_:))))
+    return vc
+  }
+
+  /// UIViewControllerの更新時にプレビューレイヤーのフレームを更新
+  func updateUIViewController(
+    _ uiViewController: UIViewController,
+    context: Context
+  ) {
+    previewLayer.frame = uiViewController.view.bounds
+  }
+}
